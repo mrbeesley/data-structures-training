7fd21601bd5b1b21b548e57a90969ddc4bfce027
Michael Beesley <mbeesley@smg.com>
2018-12-03 17:26:53 -0600

    node chaining project complete

---
+++ node-chaining/Node.cs
@@ -0,0 +1,5 @@
+public class Node
+{
+    public int Value { get; set; }
+    public Node Next { get; set; }
+}
---
+++ node-chaining/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace node_chaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +------+------+
+            // |  3   | null +
+            // +------+------+
+            var first = new Node { Value = 3 };
+
+            // +------+------+  +------+------+
+            // |  3   | null +  |  5   | null +
+            // +------+------+  +------+------+
+            var middle = new Node { Value = 5 };
+
+            // +------+------+  +------+------+
+            // |  3   |  *---+->|  5   | null +
+            // +------+------+  +------+------+
+            first.Next = middle;
+
+            // +------+------+  +------+------+  +------+------+
+            // |  3   |  *---+->|  5   | null +  |  7   | null +
+            // +------+------+  +------+------+  +------+------+
+            var last = new Node { Value = 7 };
+
+            // +------+------+  +------+------+  +------+------+
+            // |  3   |  *---+->|  5   |  *---+->|  7   | null +
+            // +------+------+  +------+------+  +------+------+
+            middle.Next = last;
+
+            // iterate over each node and print the value
+            PrintList(first);
+
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
