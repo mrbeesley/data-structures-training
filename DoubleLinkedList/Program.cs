fc601651a2a9853bd449b4bdb222933c4caaa548
Michael <mrbeesley@gmail.com>
2018-12-06 10:12:05 -0600

    finished double linked list

--- DoubleLinkedList/Program.cs
+++ DoubleLinkedList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DoubleLinkedList
 {
@@ -6,7 +7,37 @@ namespace DoubleLinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new LinkedList<int>();
+            list.Add(11);
+            list.Add(9);
+            list.Add(7);
+            list.Add(5);
+            list.Add(3);
+            list.Add(1);
+
+            Console.WriteLine("Enumerate List: 1,3,5,7,9,11");
+            Printist(list);
+
+
+            Console.WriteLine("remove 5 and 7");
+            list.Remove(5);
+            list.Remove(7);
+            Console.WriteLine("Enumerate List: 1,3,9,11");
+            Printist(list);
+
+            Console.WriteLine("Add 5 and 7 back in");
+            list.Add(5);
+            list.Add(7);
+            Console.WriteLine("Enumerate List: 7,5,1,3,9,11");
+            Printist(list);
+        }
+
+        private static void Printist(LinkedList<int> list)
+        {
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
